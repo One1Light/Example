b39dcb53f61b13c6ae5aa4cc84b8675a81fb68d8
Zhao_Zhao <>
2018-04-26 14:22:52 +0800

    update

---
+++ Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Zhao_Zhao on 2018/4/26.
+//  Copyright © 2018年 ZiRoom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
